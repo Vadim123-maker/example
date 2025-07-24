90a089038c301b9da601cef74a51367e4c682f4b
Vadim123-maker <vadimprikazchik11@gmail.com>
2025-07-24 11:42:06 +0500

    Add files via upload

---
+++ example/main.js
@@ -0,0 +1,91 @@
+// 1 задание:
+// let a = 5;
+// let b = 2;
+// let c = 5/2;
+
+// alert(c);
+
+
+
+// 2 задание:
+// let random = (Math.round(Math.random() * 10))
+// alert(random)
+
+
+// 3 задание:
+// let a =12.510;
+// alert(Math.floor(a))
+
+
+// 4 задание:
+// let str = "MARS IT SCHOOL";
+// let mars = str.replace(/\s/g, ``).length;
+// alert(mars);
+
+
+// 5 задание:
+    // for (let i = 0; i < 10; i++) {
+    //     console.log("MARS IT SCHOOL");
+    // }
+
+
+// 6 задание:
+// let harflar = ["a" , "b" , "c", "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ];
+// let nameindex = [21, 0, 3, 8, 12];
+// let name = '';
+// for (let index = 0; index < nameindex.length; index++) {
+//     name += harflar[nameindex[index]];
+// }
+// console.log(name);
+
+// 7 задание:
+// let age = Number(prompt("Введите ваш возраст:"));
+
+// if (age > 18) {
+//     alert("Возраст больше 18 лет");
+// } else if (age < 18) {
+//     alert("Возраст меньше 18 лет");
+// } else {
+//     alert("Возраст равен 18 годам");
+// }
+
+// 8 задание:
+// ...existing code...
+
+// 8 задание:
+// let name = prompt("Введите свое имя");
+// if (name) {
+//     let reversed = name.split('').reverse().join('');
+//     console.log(reversed);
+// }
+
+// 9 задание:
+// const userName = prompt("Введите ваше имя");
+// const userSurName = prompt("Введите вашу фамилию");
+// const userAge = prompt("Введите ваш возвраст");
+
+// const newTitle = document.createElement("h3");
+// newTitle.textContent = `Имя: ${userName}`;
+// newTitle.classList.add("title");
+
+// const newSurName = document.createElement("p");
+// newSurName.textContent = `Фамилия: ${userSurName}`;
+// newSurName.classList.add("surname");
+
+// const newSpan = document.createElement("span");
+// newSpan.textContent = `Возраст: ${userAge}`;
+// newSpan.classList.add("age");
+
+// const container = document.createElement("div");
+// container.classList.add("user-block");
+// container.append(newTitle, newSurName, newSpan);
+
+// document.body.append(container);
+
+
+// 10 задание:
+// const numbers = [1,2,3,4,5,6,7,8,9,10];
+// const evenNumbers = numbers.filter(num => num % 2 === 0);
+// console.log(evenNumbers);
+
+
